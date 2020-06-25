871ec53d3e7ebee095059aa9809ebbaf35ae0ca0
DraskoAndjelkovicSingidunum <65674693+DraskoAndjelkovicSingidunum@users.noreply.github.com>
2020-06-25 19:15:10 +0200

    Implement statically served videos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { StorageConfig } from 'config/storage.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(StorageConfig.video.destination, {
+    prefix: StorageConfig.video.urlPrefix,
+    maxAge: StorageConfig.video.maxAge,
+    index: false,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
